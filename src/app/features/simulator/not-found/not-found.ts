8ca77560ca5bf3fc6042c24fe6482e2b43343db5
Shriteen <Shriteen@users.noreply.github.com>
2026-08-01 22:08:49 +0530

    Styling of Error page

--- src/app/features/simulator/not-found/not-found.ts
+++ src/app/features/simulator/not-found/not-found.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from "@angular/core";
 import { Router, RouterLink } from "@angular/router";
 import { ModalService } from "@core/modal/ModalService";
+import { LucideHouse, LucidePlus } from "@lucide/angular";
 import { CreateDialog } from "@shared/create-dialog/create-dialog";
+
 
 @Component({
   selector: "app-not-found",
-  imports: [RouterLink],
+  imports: [RouterLink, LucideHouse, LucidePlus],
   templateUrl: "./not-found.html",
   styleUrl: "./not-found.css",
 })
